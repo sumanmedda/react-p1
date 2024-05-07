d3070332d477027f067b84b723aace13256c0554
sumanmedda <sumanmedda33@gmail.com>
2024-05-07 12:31:03 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import CustomButton from "./CustomButton"
 
 function App(){
   return (
     <div>
       <h1>Hello World</h1>
+      <CustomButton />
     </div>
   )
 }
---
+++ src/CustomButton.jsx
@@ -0,0 +1,11 @@
+function CustomButton(){
+return(
+    <div className="custom-button">
+        <button>
+            <span>Get Started</span>
+        </button>
+    </div>
+)
+}
+
+export default CustomButton;
